81691af34e60b0c8faf1a60eef2c89ec2c219667
Valentina Soto <valentinasoto9822@gmail.com>
2022-05-11 19:12:31 -0500

    configuration paths of the components and validations of these

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,43 @@
+import{ NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+
+const routes: Routes = [
+    {
+        path: '',
+        component: PorPaisComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'capital',
+        component: PorCapitalComponent
+    },
+    {
+        path: 'region',
+        component: PorRegionComponent
+    },
+    {
+        path: 'pais/:id',
+        component: VerPaisComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot( routes )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule {}
