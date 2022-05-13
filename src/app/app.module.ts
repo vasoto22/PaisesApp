c7874e87d1ba5dcb3836930764854b6c57f2b059
Valentina Soto <valentinasoto9822@gmail.com>
2022-05-13 11:24:20 -0500

    Construction of Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
@@ -12,9 +13,11 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
     PaisModule,
     SharedModule,
-    AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+
+  termino: string = 'Hola Mundo';
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+ buscar() {
+   console.log(this.termino);
+ }
 
 }
